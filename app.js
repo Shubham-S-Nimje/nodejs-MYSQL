d5a1c105cc944840da1a2f82336f781a29445278
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2023-07-27 00:16:17 +0530

    Making SQL easy with Sequelize

--- app.js
+++ app.js
@@ -2,42 +2,42 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const errorController = require("./controllers/error");
 
 const app = express();
 
-function getProducts() {
-  db.execute("SELECT * FROM products").then(([rows, fileData]) => {
-    console.log(rows);
-  });
-}
+// function getProducts() {
+//   db.execute("SELECT * FROM products").then(([rows, fileData]) => {
+//     console.log(rows);
+//   });
+// }
 // getProducts();
 
-function insertProduct() {
-  const query =
-    "INSERT INTO products (title, description, imageUrl, price) VALUES (?, ?, ?, ?)";
-  const values = [
-    "Shoe",
-    "This is an awesome shoe!..",
-    "url2", 1999,
-  ];
-  db.execute(query, values).then((result) => {
-    console.log(result);
-  });
-}
+// function insertProduct() {
+//   const query =
+//     "INSERT INTO products (title, description, imageUrl, price) VALUES (?, ?, ?, ?)";
+//   const values = [
+//     "Shoe",
+//     "This is an awesome shoe!..",
+//     "url2", 1999,
+//   ];
+//   db.execute(query, values).then((result) => {
+//     console.log(result);
+//   });
+// }
 // insertProduct();
 
-function deleteProducts(id) {
-    const query =
-      "DELETE FROM products WHERE products.id = ?";
-    const values = [2];
-    db.execute(query, values).then(([row, fileData]) => {
-      console.log(row);
-    }).catch((err) => console.log(err));
-  }
-  deleteProducts();
+// function deleteProducts(id) {
+//     const query =
+//       "DELETE FROM products WHERE products.id = ?";
+//     const values = [2];
+//     db.execute(query, values).then(([row, fileData]) => {
+//       console.log(row);
+//     }).catch((err) => console.log(err));
+//   }
+//   deleteProducts();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -52,5 +52,15 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
+
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000);
--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "nodejs-start",
+//   password: "2112",
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "2112", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "nodejs-start",
-  password: "2112",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
